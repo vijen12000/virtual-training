c71e74bce43db0bba8b705a57eb9bb63f348cabf
Vijender Kumar <vijender.kumar@ihsmarkit.com>
2022-01-14 21:55:04 +0530

    Display spin icon next to favorite to indicate processing untill parent sends callback

--- src/components/Speaker/SpeakerFavorite.js
+++ src/components/Speaker/SpeakerFavorite.js
@@ -1,12 +1,14 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 const SpeakerFavorite = ({favorite, onFavoriteToggle}) => {
+    const [inTransition, setInTransition] = useState(false)
     function doneCallback(params) {
-        console.log('In parent :done callback')
+        setInTransition(false)
     }
     return (
         <div className='action padB1'>
             <span onClick={function (params) {
+                setInTransition(true)
                 return onFavoriteToggle(doneCallback)
             }}>
                 <i
@@ -14,6 +16,10 @@ const SpeakerFavorite = ({favorite, onFavoriteToggle}) => {
                     ? "fa fa-star orange"
                     : "fa fa-star-o ornage"}></i>{" "}
                 Favorite{" "}
+                {inTransition?
+                    <span className="fa fa-circle-notch fa-spin"></span>    
+                    :null
+                }
             </span>
         </div>
     )
